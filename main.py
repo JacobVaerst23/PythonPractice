34334f754641a86146160018505e5e71ab3e916e
JacobVaerst23 <816769@mystma.org>
2021-09-13 09:47:51 -0500

    Top 5 Movies

---
+++ main.py
@@ -0,0 +1,10 @@
+#Print 1st movie
+print("Fast and Furious 5")
+#Pront 2nd movie
+print("Fast and Furious 7")
+#Print 3rd movie
+print("Star Wars series")
+#Print 4th movie
+print("Jumanji")
+#Print 5th movie
+print("Suicide Squad")
