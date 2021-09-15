fa662092c19af2a7273f35c02fcdf5dc28af2f40
JacobVaerst23 <816769@mystma.org>
2021-09-15 09:13:25 -0500

    Top five lists

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#define the function
 def topFiveMovies():
     #print the first movie!
     print("Fast and Furious 5")
@@ -14,5 +15,80 @@ def topFiveMovies():
     #print fifth movie
     print("Suicide Squad")
 
-print("these are my top five movies")
+def topFIveSports():
+
+    #print first sport
+    print("Basketball")
+
+    #print second sport
+    print("Football")
+
+    #print third sport
+    print("Baseball")
+
+    #print fourth sport
+    print("Hockey")
+
+    #print fifth sport
+    print("Track")
+
+def topFiveClasses():
+
+    #print first class
+    print("POE")
+
+    #print second class
+    print("Intro to programming")
+
+    #print third class
+    print("Pre Calculus")
+
+    #print fourth class
+    print("English")
+
+    #print fifth class
+    Print("Physics")
+
+def topFiveCars():
+
+    #print first car
+    print("McLaren")
+
+    #print second car
+    print("Porche")
+
+    #print third car
+    print("Tesla")
+
+    #print fourth car
+    print("Supra")
+
+    #print fifth car
+    print("C8 Corvette")
+
+def topFiveRestaurants():
+
+    #print first restaurant
+    print("Chipotle")
+
+    #print second restaurant
+    print("Culvers")
+
+    #print third restaurant
+    print("Applebees")
+
+    #print fourth restaurant
+    print("The Big Bore")
+
+    #print fifth restaurant
+    print("Olive Garden")
+
 topFiveMovies()
+
+topFiveSports()
+
+topFiveClasses()
+
+topFiveCars()
+
+topFiveRestaurants()
