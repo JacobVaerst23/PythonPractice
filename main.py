0b06bc9d2002ae0b694dcbb0315da91afcd7284e
JacobVaerst23 <816769@mystma.org>
2021-09-24 09:48:33 -0500

    If statements

--- main.py
+++ main.py
@@ -1,105 +1,12 @@
-#define the function
-def topFiveMovies():
-    
-    #print the first movie!
-    print("Fast and Furious 5")
+def greaterThan10(x):
+    if x > 10:
+         return "x is greater than 10."
 
-    #print second movie
-    print("Fast and Furious 7")
+    elif x == 10:
+        return "x equals 10"
 
-    #Print third movie
-    print("Star Wars Series")
+    else:
 
-    #Print fourth movie
-    print("Jumanji")
+        return "x is not greater than 10"
 
-    #print fifth movie
-    print("Suicide Squad")
-
-def topFIveSports():
-
-    #print first sport
-    print("Basketball")
-
-    #print second sport
-    print("Football")
-
-    #print third sport
-    print("Baseball")
-
-    #print fourth sport
-    print("Hockey")
-
-    #print fifth sport
-    print("Track")
-
-def topFiveClasses():
-
-    #print first class
-    print("POE")
-
-    #print second class
-    print("Intro to programming")
-
-    #print third class
-    print("Pre Calculus")
-
-    #print fourth class
-    print("English")
-
-    #print fifth class
-    Print("Physics")
-
-def topFiveCars():
-
-    #print first car
-    print("McLaren")
-
-    #print second car
-    print("Porche")
-
-    #print third car
-    print("Tesla")
-
-    #print fourth car
-    print("Supra")
-
-    #print fifth car
-    print("C8 Corvette")
-
-def topFiveRestaurants():
-
-    #print first restaurant
-    print("Chipotle")
-
-    #print second restaurant
-    print("Culvers")
-
-    #print third restaurant
-    print("Applebees")
-
-    #print fourth restaurant
-    print("The Big Bore")
-
-    #print fifth restaurant
-    print("Olive Garden")
-
-topFiveMovies()
-
-topFiveSports()
-
-topFiveClasses()
-
-topFiveCars()
-
-topFiveRestaurants()
-
-#defube a function that outputs your name
-#folloed by "my name is"
-def myNameIs(bob):
-
-    #output string
-    print(bob + "is my name")
-
-#run function
-myNameIs("Jacob")
+print(greaterThan10(10))
