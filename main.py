1eb0328551d0a0b8ceec18cab8a3234cb625a7d9
JacobVaerst23 <816769@mystma.org>
2021-09-27 09:47:39 -0500

    10 definitions

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-def greaterThan10(x):
-    if x > 10:
-         return "x is greater than 10."
+def define(word):
 
-    elif x == 10:
-        return "x equals 10"
+    word = word.lower()
+
+
+    if word == "sports":
+        return "An activity involving physical activity"
+
+    elif word == "gear":
+        return "A circular object that has teeth that rotates"
+
+    elif word == "efficiency": 
+        return "calculation of how effective something is"
+
+    elif word == "effort":
+        return "the force you are applying to try and overcome resistance"
+
+    elif word == "resistance":
+        return "effect exerted by one material object on another"
+
+    elif word == "pulley":
+        return "a simple machine that consists of a rope that fits into a groove in a wheel"
+
+    elif word == "basketball":
+        return "a type of sport that uses a ball and a 10 foot high hoop"
+
+    elif word == "baseball":
+        return "a type of sport that uses a small ball, a glove, and a bat"
+
+    elif word == "dictionary":
+        return "a book full of definitions"
+
+    elif word == "boolean":
+        return "data that is either true or false"
 
     else:
+        return word + "is not defined in this dictionary"
 
-        return "x is not greater than 10"
-
-print(greaterThan10(10))
+print(define("Dictionary"))
