1a0c81f64672fb31893586b2f2498696459fd8c8
JacobVaerst23 <816769@mystma.org>
2021-09-29 09:10:52 -0500

    Python Pratice

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
-def define(word):
-
-    word = word.lower()
-
-
-    if word == "sports":
-        return "An activity involving physical activity"
-
-    elif word == "gear":
-        return "A circular object that has teeth that rotates"
-
-    elif word == "efficiency": 
-        return "calculation of how effective something is"
-
-    elif word == "effort":
-        return "the force you are applying to try and overcome resistance"
-
-    elif word == "resistance":
-        return "effect exerted by one material object on another"
-
-    elif word == "pulley":
-        return "a simple machine that consists of a rope that fits into a groove in a wheel"
-
-    elif word == "basketball":
-        return "a type of sport that uses a ball and a 10 foot high hoop"
-
-    elif word == "baseball":
-        return "a type of sport that uses a small ball, a glove, and a bat"
-
-    elif word == "dictionary":
-        return "a book full of definitions"
-
-    elif word == "boolean":
-        return "data that is either true or false"
+def evenOrOdd(x):
+    if x % 2 == 0:
+        return True
 
     else:
-        return word + "is not defined in this dictionary"
+        return False
 
-print(define("Dictionary"))
+print(evenOrOdd(69420))
